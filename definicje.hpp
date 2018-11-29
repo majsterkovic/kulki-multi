e2fda89e7af1c508005cb3355f29399718434bd4
majsterkovic <mariusz8h@o2.pl>
2018-11-29 22:52:10 +0100

    Add files via upload
    podział programu na pliki

---
+++ definicje.hpp
@@ -0,0 +1,17 @@
+#ifndef definicje_hpp
+#define definicje_hpp
+
+#include <cstdlib> // losowanko
+#include <iostream> //cin cout
+#include <iomanip> // ³adne wyœwietlanie
+#include <chrono> // zatrzymywanie
+#include <ctime> // losowanie
+#include <thread> // zatrzymywanie
+#include <random> // losowanko (u mnie nie potrzeba ale w szkole tak)
+#include <windows.h> // beep
+
+using namespace std;
+int sprawdzenie(unsigned char plansza[12][12]);
+
+
+#endif
---
+++ sprawdzenie.cpp
@@ -0,0 +1,45 @@
+#include "definicje.hpp"
+
+extern bool wygrana;
+int sprawdzenie(unsigned char plansza[12][12])
+{
+    for(int j=0; j<11; j++)
+    {
+
+
+
+    for(int i=0;i<11;i++)
+    {
+        if
+        (
+            ((plansza[j][i]==plansza[j][i+1]) && (plansza[j][i+2] == plansza[j][i+3]) && (plansza[j][i]==plansza[j][i+3]) && (plansza[j][i]=='X')) ||
+            ((plansza[i][j]==plansza[i+1][j]) && (plansza[i+2][j] == plansza[i+3][j]) && (plansza[i][j]==plansza[i+3][j]) && (plansza[i][j]=='X')) ||
+            ((plansza[i][j]==plansza[i+1][j+1]) && (plansza[i+2][j+2] == plansza[i+3][j+3]) && (plansza[i][j]==plansza[i+3][j+3]) && (plansza[i][j]=='X')) ||
+            ((plansza[i][j]==plansza[i-1][j+1]) && (plansza[i-2][j+2] == plansza[i-3][j+3]) && (plansza[i][j]==plansza[i-3][j+3]) && (plansza[i][j]=='X'))
+        )
+           {
+              wygrana = true;
+              cout << "Wygrales! Gitara siema!" << endl;
+           }
+
+        else if
+        (
+            ((plansza[j][i]==plansza[j][i+1]) && (plansza[j][i+2] == plansza[j][i+3]) && (plansza[j][i]==plansza[j][i+3]) && (plansza[j][i]=='O')) ||
+            ((plansza[i][j]==plansza[i+1][j]) && (plansza[i+2][j] == plansza[i+3][j]) && (plansza[i][j]==plansza[i+3][j]) && (plansza[i][j]=='O')) ||
+            ((plansza[i][j]==plansza[i+1][j+1]) && (plansza[i+2][j+2] == plansza[i+3][j+3]) && (plansza[i][j]==plansza[i+3][j+3]) && (plansza[i][j]=='O')) ||
+            ((plansza[i][j]==plansza[i-1][j+1]) && (plansza[i-2][j+2] == plansza[i-3][j+3]) && (plansza[i][j]==plansza[i-3][j+3]) && (plansza[i][j]=='O'))
+        )
+           {
+              wygrana = true;
+              cout << "Wygral komputer ty slabiaku" << endl;
+           }
+           /*jak działa to nie ruszać*/
+
+
+
+
+    }
+
+    }
+return wygrana;
+}
