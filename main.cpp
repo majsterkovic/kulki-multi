43f93b4e65ccb38ecfe979685595360bdbf4c7d4
majsterkovic <mariusz8h@o2.pl>
2018-11-29 14:15:36 +0100

    Update main.cpp
    sprawdzenie poziomo i pionowo działa

--- main.cpp
+++ main.cpp
@@ -3,11 +3,56 @@
 #include <chrono>
 #include <ctime>
 #include <thread>
+#include <random>
 
 //kod 0 = 48
 //kod ░ = 176
 using namespace std;
 bool wygrana = false;
+
+int sprawdzenie(unsigned char plansza[12][12])
+{
+    for(int j=0; j<11; j++)
+    {
+
+
+
+    for(int i=0;i<11;i++)
+    {
+        if
+        (
+            ((plansza[j][i]==plansza[j][i+1]) && (plansza[j][i+2] == plansza[j][i+3]) && (plansza[j][i]==plansza[j][i+3]) && (plansza[j][i]=='X')) ||
+            ((plansza[i][j]==plansza[i+1][j]) && (plansza[i+2][j] == plansza[i+3][j]) && (plansza[i][j]==plansza[i+3][j]) && (plansza[i][j]=='X')) ||
+            ((plansza[i][j]==plansza[i+1][j+1]) && (plansza[i+2][j+2] == plansza[i+3][j+3]) && (plansza[i][j]==plansza[i+3][j+3]) && (plansza[i][j]=='X'))
+
+        )
+           {
+              wygrana = true;
+              cout << "Wygrales! Gitara siema!" << endl;
+           }
+
+           if
+        (
+            ((plansza[j][i]==plansza[j][i+1]) && (plansza[j][i+2] == plansza[j][i+3]) && (plansza[j][i]==plansza[j][i+3]) && (plansza[j][i]=='O')) ||
+            ((plansza[i][j]==plansza[i+1][j]) && (plansza[i+2][j] == plansza[i+3][j]) && (plansza[i][j]==plansza[i+3][j]) && (plansza[i][j]=='O')) ||
+            ((plansza[i][j]==plansza[i+1][j+1]) && (plansza[i+2][j+2] == plansza[i+3][j+3]) && (plansza[i][j]==plansza[i+3][j+3]) && (plansza[i][j]=='O'))
+
+        )
+           {
+              wygrana = true;
+              cout << "Wygral komputer ty smieciu" << endl;
+           }
+           /*jak działa to nie ruszać*/
+
+
+
+
+    }
+
+    }
+return wygrana;
+}
+
 
 void kropki(int l)
 {
@@ -17,18 +62,18 @@ void kropki(int l)
     }
 }
 
-int wypelnijplansze(unsigned char plansza[16][16])
+int wypelnijplansze(unsigned char plansza[12][12])
 {
-   for(int g=0; g<16; g++)
+   for(int g=0; g<12; g++)
         {
-        for(int h=0; h<16; h++)
+        for(int h=0; h<12; h++)
         {
             plansza[g][h]='0';
         }
     }
 }
 
-void planszuj(unsigned char plansza[16][16])
+void planszuj(unsigned char plansza[12][12])
 {
     cout << " ";
     for(int f=0; f<10; f++)
@@ -37,10 +82,10 @@ void planszuj(unsigned char plansza[16][16])
             }
     cout << "|" << endl;
 
-    for(int g=3; g<13; g++)
+    for(int g=1; g<11; g++)
         {
-            cout << g-3;
-            for(int h=3; h<13; h++)
+            cout << g-1;
+            for(int h=1; h<11; h++)
             {
                 cout << setw(1) << "|" << plansza[g][h];
             }
@@ -49,7 +94,7 @@ void planszuj(unsigned char plansza[16][16])
     cout << endl;
 }
 
-void gracz(unsigned char plansza[16][16])
+void gracz(unsigned char plansza[12][12])
 {
     int x;
     int y;
@@ -59,11 +104,11 @@ void gracz(unsigned char plansza[16][16])
     cout << "Gdzie chcesz postawic swoja kulke? Podaj wspolrzedne [wiersz i kolumna]: ";
     cin >> y;
     cin >> x;
-    x = x+3;
-    y = y+3;
-    if((plansza[y][x] == 176) && (plansza[y+1][x] == '0' || plansza[y+1][x] == 'O' || plansza[y+1][x] == 'Q'))
+    x = x+1;
+    y = y+1;
+    if((plansza[y][x] == 176) && (plansza[y+1][x] == '0' || plansza[y+1][x] == 'O' || plansza[y+1][x] == 'X'))
         {
-            plansza[y][x] = 'Q';
+            plansza[y][x] = 'X';
             b = 0;
         }
     else
@@ -73,16 +118,16 @@ void gracz(unsigned char plansza[16][16])
     }while(b==1);
 }
 
-int komputer(unsigned char plansza[16][16])
+int komputer(unsigned char plansza[12][12])
 {
     int x, y;
     int M = 0;
     bool b = 1;
     do{
-    x = (std::rand() % 10) + 3;
-    y = (std::rand() % 10) + 3;
+    x = (std::rand() % 10) + 1;
+    y = (std::rand() % 10) + 1;
 
-    if((plansza[y][x] == 176) && (plansza[y+1][x] == '0' || plansza[y+1][x] == 'O' || plansza[y+1][x] == 'Q'))
+    if((plansza[y][x] == 176) && (plansza[y+1][x] == '0' || plansza[y+1][x] == 'O' || plansza[y+1][x] == 'X'))
         {
             plansza[y][x] = 'O';
             b = 0;
@@ -104,12 +149,12 @@ int main()
 {
     srand(time(NULL));
     cout << "Witaj w grze w kulki xD" << endl;
-    unsigned char plansza[16][16];
+    unsigned char plansza[12][12];
     wypelnijplansze(plansza);
 
-    for(int g=3; g<13; g++)
+    for(int g=1; g<11; g++)
         {
-        for(int h=3; h<13; h++)
+        for(int h=1; h<11; h++)
         {
             plansza[g][h] = 176;
             cout << "|" << plansza[g][h];
@@ -120,14 +165,17 @@ int main()
 
     do{
 
+    cout << "\a";
     gracz(plansza);
     planszuj(plansza);
+    sprawdzenie(plansza);
+    if(wygrana==true) break;
     komputer(plansza);
     planszuj(plansza);
+    sprawdzenie(plansza);
     cout << "Uwaga, czyszczenie konsoli za 8 sekund!" << endl;
     kropki(3);
-    system("cls");
-
+    // system("cls");
     }while(wygrana==false);
 
     return 0;
