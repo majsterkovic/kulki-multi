858281a5d01557f83cd663559991c0bc02a6ff54
majsterkovic <mariusz8h@o2.pl>
2018-11-30 15:13:14 +0100

    Update sprawdzenie.cpp
    niby działa

--- sprawdzenie.cpp
+++ sprawdzenie.cpp
@@ -1,29 +1,47 @@
 #include "definicje.hpp"
 
 extern bool wygrana;
+void pokazwygrana(unsigned char plansza[12][12], int i, int j)
+{
+   if(plansza[i][j]=='X')
+        {
+            cout << "Wygral gracz X!" << endl;
+        }
+    else if(plansza[i][j]=='O')
+        {
+            cout << "Wygral gracz O!" << endl;
+        }
+}
+
+
+
 int sprawdzenie(unsigned char plansza[12][12])
 {
-    for(int j=0; j<11; j++)
-    {
+for(int j=0; j<11; j++)
+{
     for(int i=0;i<11;i++)
     {
         if
         (
-            ((plansza[j][i]==plansza[j][i+1]) && (plansza[j][i+2] == plansza[j][i+3]) && (plansza[j][i]==plansza[j][i+3]) && ((plansza[j][i]=='X') || (plansza[j][i]=='O'))) ||
             ((plansza[i][j]==plansza[i+1][j]) && (plansza[i+2][j] == plansza[i+3][j]) && (plansza[i][j]==plansza[i+3][j]) && ((plansza[i][j]=='X') || (plansza[i][j]=='O'))) ||
             ((plansza[i][j]==plansza[i+1][j+1]) && (plansza[i+2][j+2] == plansza[i+3][j+3]) && (plansza[i][j]==plansza[i+3][j+3]) && ((plansza[i][j]=='X') || (plansza[i][j]=='O'))) ||
             ((plansza[i][j]==plansza[i-1][j+1]) && (plansza[i-2][j+2] == plansza[i-3][j+3]) && (plansza[i][j]==plansza[i-3][j+3]) && ((plansza[i][j]=='X') || (plansza[i][j]=='O')))
         )
            {
               wygrana = true;
-              if(plansza[i][j]=='X')
-              cout << "Wygrales! Gitara siema!" << endl;
-              else if(plansza[i][j]=='O')
-              cout << "Wygral komputer ty slabiaku!" << endl;
+              pokazwygrana(plansza, i, j);
+
+
            }
+        if((plansza[j][i]==plansza[j][i+1]) && (plansza[j][i+2] == plansza[j][i+3]) && (plansza[j][i]==plansza[j][i+3]) && ((plansza[j][i]=='X') || (plansza[j][i]=='O')))
+        {
+            wygrana = true;
+            pokazwygrana(plansza, j, i);
+        }
 
            /*jak działa to nie ruszać*/
     }
-    }
+}
+
 return wygrana;
 }
