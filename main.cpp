3d781f0b1c1732010d660cb4e8a64dec2809788c
majsterkovic <mariusz8h@o2.pl>
2019-05-17 00:15:41 +0200

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -6,103 +6,92 @@ bool wygrana = false;
 int x;
 int y;
 
-int gracz(unsigned char plansza[12][12], int gracz)
-{
-
-    bool b = 1;
-
-    planszuj(plansza);
-
-    do{
-    cout << "Gdzie chcesz postawic swoja kulke? Podaj wspolrzedne [wiersz i kolumna]: ";
-    cin >> y;
-    cin >> x;
-    x = x+1;
-    y = y+1;
-    if((plansza[x][y] == 176) && (plansza[x+1][y] == '0' || plansza[x+1][y] == 'O' || plansza[x+1][y] == 'X'))
-        {
-            if(gracz == 1)
-                plansza[x][y] = 'X';
-            else if(gracz ==2)
-                plansza[x][y] = 'O';
-                b = 0;
-            Beep(1000,1000);
-            planszuj(plansza);
-        }
-    else
-    {
-            cout << "Sprobuj jeszcze raz: " << endl;
-    }
-
-    }while(b==1);
-}
-
+char Xx = 'X';
+char Oo = 'O';
 
 
 int main()
 {
+     char pomoc;
     srand(time(NULL));
+
     char zgoda = 'T';
 
     while(zgoda=='T')
-    {
-
-    system("cls");
-    wygrana == false;
-    cout << "Witaj w grze w kulki xD" << endl << endl << "\a" ;
-    unsigned char plansza[12][12];
-    wypelnijplansze(plansza);
-
-    for(int g=1; g<11; g++)
         {
-        for(int h=1; h<11; h++)
+
+        system("cls");
+        wygrana = false;
+        cout << "Witaj w grze w kulki xD" << endl << endl/* << "\a" */ ;
+        unsigned char plansza[12][12];
+        wypelnijplansze(plansza);
+
+        for(int g=1; g<11; g++) //tło
+            {
+                for(int h=1; h<11; h++)
+                {
+                    plansza[g][h] = 176;
+                }
+            }
+
+        wybierzskin(1);
+        std::this_thread::sleep_for(std::chrono::milliseconds(1000));
+        wybierzskin(2);
+
+        kropki(4);
+        system("cls");
+
+        do
         {
-            plansza[g][h] = 176;
+
+            gracz(plansza, 1);
+            sprawdzenie(plansza);
+            if(wygrana==true) break;
+
+            else
+            {
+           // kropki(2);
+            system("cls");
+            }
+
+        if(!sprawdzPlansze(plansza))
+            {
+            cout << "Koniec gry, plansza jest pelna" << endl;
+            break;
+            }
+
+
+        gracz(plansza, 2);
+        sprawdzenie(plansza);
+
+        if(!sprawdzPlansze(plansza))
+            {
+            cout << "Koniec gry, plansza jest pelna" << endl;
+            break;
+            }
+
+        if(wygrana == false)
+        {
+            // kropki(2);
+            system("cls");
         }
-    }
-
-    do{
-
-    gracz(plansza, 1);
-    sprawdzenie(plansza);
-    if(wygrana==true) break;
-
-    else
-    {
-    cout << "Uwaga, czyszczenie konsoli za 2 sekundy" << endl;
-    kropki(2);
-    system("cls");
-    }
 
 
-    gracz(plansza, 2);
-    sprawdzenie(plansza);
+        }while(wygrana==false);
 
-    if(wygrana == false)
-    {
-    cout << "Uwaga, czyszczenie konsoli za 2 sekundy!" << endl;
-    kropki(2);
-    system("cls");
-    }
+        cout << "Czy chcesz zagrac jeszcze raz? (T/N): " << endl;
 
+        do{
+            cin >> zgoda;
 
-    }while(wygrana==false);
+            if(zgoda!='N' && zgoda!='T')
+                cout << "Wybierz T lub N!" << endl;
 
-    cout << "Czy chcesz zagrac jeszcze raz? (T/N): " << endl;
-
-    do{
-        cin >> zgoda;
-
-        if(zgoda!='N' && zgoda!='T')
-            cout << "Wybierz T lub N!" << endl;
-
-    }while(zgoda!='N' && zgoda!='T');
-
+        }while(zgoda!='N' && zgoda!='T');
 
     }
 
     cout << "Dziekuje za gre!";
-    char pomoc;
     cin >> pomoc;
     return 0;
 }
